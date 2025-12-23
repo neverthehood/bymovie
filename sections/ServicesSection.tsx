ce8d902e25e1cee42a9a2ae623f9bb0ade9f9436
duskrider32 <duskrider32@gmail.com>
2025-12-23 18:40:41 +0300

    Update ServicesSection.tsx

--- sections/ServicesSection.tsx
+++ sections/ServicesSection.tsx
@@ -37,7 +37,7 @@ export default function ServicesSection() {
 
       // скорость прокрутки
       // mobile — медленно, desktop — быстрее
-      const speed = isMobile ? 1.2 : 2.5;
+      const speed = isMobile ? 1.8 : 2.5;
 
       const progress = (scrollY - sectionTop) / (vh * speed);
 
@@ -149,7 +149,7 @@ export default function ServicesSection() {
                       window.scrollTo({
                         top:
                           sectionRef.current!.offsetTop +
-                          i * window.innerHeight * 1,
+                          i * window.innerHeight * 1.2,
                         behavior: "smooth",
                       })
                     }
