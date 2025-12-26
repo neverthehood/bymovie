bc68006914765bc6f1a5f28565ed837df29997d1
duskrider32 <duskrider32@gmail.com>
2025-12-26 17:54:54 +0300

    Update GallerySlider.tsx

--- app/vp/GallerySlider.tsx
+++ app/vp/GallerySlider.tsx
@@ -46,7 +46,7 @@ export default function GallerySlider({ images }: { images: Slide[] }) {
   // =========================
   // CONFIG
   // =========================
-  const gap = isDesktop ? 16 : 18;
+  const gap = isDesktop ? 16 : 14;
 
   const snapW = 300;
   const snapH = 480;
